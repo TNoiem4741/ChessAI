73850b4c953146b8f35b9cc255c17e44030032b6
TNoiem4741 <90115008+TNoiem4741@users.noreply.github.com>
2024-01-05 21:37:27 -0500

    Simplified main

--- game/game.py
+++ game/game.py
@@ -1,3 +1,15 @@
 import pygame 
-import sqaure 
-def create_board():
+from game.board import board
+
+class game:
+    def start_game(screen):
+        chess_matrix = board.Board.create_board() #create a game board
+        board.Board.show_board(chess_matrix, screen) #display board game onto screen
+        clock = pygame.time.Clock() #frame rate object
+        running = True
+        # game loop
+        while running:
+            clock.tick(60) # 60 fps cap
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT: running = False # quit game
+            pygame.display.update() # update screen
--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import pygame
-from game.board import board
+from game import game
 
 class Main:
     def __init__(self):
-        # pygame setup
         pygame.init() # init pygame
-        pygame.display.set_caption("ChessAI") #set window title
-        self.screen = pygame.display.set_mode((800, 800)) #set dimensions
-        chess_matrix = board.Board.create_board() #create a game board
-        clock = pygame.time.Clock() #frame rate object
-        running = True
+        self.screen = pygame.display.set_mode((800, 800)) #set screen dimensions
+        pygame.display.set_caption("ChessAI") #set screen title
+        game.game.start_game(self.screen) #begin the game
         
-        # game loop
-        while running:
-            for event in pygame.event.get(): 
-                if event.type == pygame.QUIT: running = False # quit game
-            board.Board.show_board(chess_matrix, self.screen) #display board game onto screen
-            clock.tick(30) # 30 fps cap
-            pygame.display.update() # update screen
 Main()
